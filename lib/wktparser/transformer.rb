1942baa6870541d7292ca9e3775957208e4c5ac3
sanzstez <sanzstez@gmail.com>
2018-07-17 10:56:18 +0300

    fix

--- lib/wktparser/transformer.rb
+++ lib/wktparser/transformer.rb
@@ -1,8 +1,8 @@
 module WKTParser
   class Transformer < Parslet::Transform
     rule(:type    => simple(:type))            { type.to_s }
-    rule(:lnumber => simple(:lnumber),
-         :rnumber => simple(:rnumber))         { [lnumber, rnumber] }
+    rule(:lnumber => simple(:lnumber) { lnumber.match(/[eE\.]/) ? Float(lnumber) : Integer(lnumber)},
+         :rnumber => simple(:rnumber){ rnumber.match(/[eE\.]/) ? Float(rnumber) : Integer(rnumber)}) { [lnumber, rnumber] }
 
     rule(:union   => subtree(:u)) do
       if u.count == 1 then
